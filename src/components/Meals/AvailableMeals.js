b78e79c1f4c3b778f0f9528cbc72c0c47fd35638
Mac <mac@Macs-MacBook-Pro.local>
2022-09-09 17:46:37 +0530

    fixes

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,6 @@
 import classes from './AvailableMeals.module.css'
-
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 
 const DUMMY_MEALS = [
     {
@@ -29,10 +30,18 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map((meals) => <li>{meals.name}</li>)
-
+    const mealsList = DUMMY_MEALS.map((meals) =>
+        <MealItem
+            key={meals.id}
+            name={meals.name}
+            description={meals.description}
+            price={meals.price}
+        />
+    )
     return <section className={classes.meals}>
-        <ul>{mealsList}</ul>
+        <Card>
+            <ul>{mealsList}</ul>
+        </Card>
     </section>
 
 }
